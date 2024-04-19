91392897fd5fd5f066a0dabe7829e35907a807ff
Rodrigo Alves Guerra <orbital@rodrigoalvesguerra.com.br>
2024-04-19 16:47:47 -0500

    Add secao 7

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// Código Não Executado !!!
+
+// Procedural
+processamento(valor1, valor2, valor3);
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento(); // Foco passou a ser o objeto.
+
+// Princípios Importantes: 
+// 1. Abstração 
+// 2. Encapsulamento 
+// 3. Herança (prototype)
+// 4. Polimorfismo
