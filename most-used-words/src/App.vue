f7895c981812f0782d3462d49f7f450980ea3e91
Rodrigo Alves Guerra <orbital@rodrigoalvesguerra.com.br>
2024-05-13 13:33:30 -0500

    add secao 30

---
+++ most-used-words/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar app color="primary">
+        <v-app-bar-title>Most Used Words</v-app-bar-title>
+      </v-app-bar>
+      <HomePage/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+export default {
+  name: 'App',
+
+  components: {
+    HomePage,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ most-used-words/vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+	"transpileDependencies": [
+		"vuetify"
+	],
+	pluginOptions: {
+		electronBuilder: {
+			nodeIntegration: true
+		}
+	}    
+}
